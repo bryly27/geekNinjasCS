5ec4a3db13cec9018912eeaec710cf50f972e833
Bryant <bryly27@sbcglobal.net>
2015-08-27 18:51:18 -0700

    updated countdown

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$(".content").hide();
 	$(".content").fadeIn(3000);
 
-	$("#countdown").countdown("2015/08/28", function(results){
+	$("#countdown").countdown("2015/09/25", function(results){
 		$("#days").text(results.strftime("%D"));
 		$("#hours").text(results.strftime("%H"));
 		$("#minutes").text(results.strftime("%M"));
